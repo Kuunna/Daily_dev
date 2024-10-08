b0ed725ca4cce3298db48097e096d92d74cd9a16
Truong Hoai Nam <truonghoainam2k2@gmail.com>
2024-10-09 02:18:25 +0300

    update 08.10.2024

---
+++ Dailydev Challenge/DailyDev/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿namespace DailyDev.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ProviderId { get; set; }
+        public string Source { get; set; }
+        public int Ttl { get; set; }
+        public string Generator { get; set; }
+        public string Docs { get; set; }
+
+        // Navigation Property
+        public virtual Provider Provider { get; set; }
+    }
+}
