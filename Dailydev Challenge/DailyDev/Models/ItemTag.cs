227ebaf1be36b93a6227340cbaff9c8c8bb0bc7f
Truong Hoai Nam <truonghoainam2k2@gmail.com>
2024-10-13 22:49:37 +0300

    update 13.10.2024

---
+++ Dailydev Challenge/DailyDev/Models/ItemTag.cs
@@ -0,0 +1,14 @@
+﻿namespace DailyDev.Models
+{
+    public class ItemTag
+    {
+        public int Id { get; set; }
+        public int ItemId { get; set; }
+        public int TagId { get; set; }
+
+        // Navigation Properties
+        public virtual Item Item { get; set; }
+        public virtual Tag Tag { get; set; }
+    }
+
+}
