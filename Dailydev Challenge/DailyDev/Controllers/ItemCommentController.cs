19c06dc020c2e40b81ef026de7ceeae5e889400a
Kunna <truonghoainam2k2@gmail.com>
2024-10-18 01:43:01 +0300

    Last update 17.10.2024

---
+++ Dailydev Challenge/DailyDev/Controllers/ItemCommentController.cs
@@ -0,0 +1,76 @@
+﻿using DailyDev.Models;
+using DailyDev.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DailyDev.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemCommentController : ControllerBase
+    {
+        private readonly ItemCommentRepository _itemCommentRepository;
+
+        public ItemCommentController(ItemCommentRepository itemCommentRepository)
+        {
+            _itemCommentRepository = itemCommentRepository;
+        }
+
+        [HttpGet("{itemId}")]
+        public async Task<ActionResult<IEnumerable<ItemComment>>> GetCommentsByItem(int itemId)
+        {
+            var comments = await _itemCommentRepository.GetByItemId(itemId);
+            return Ok(comments);
+        }
+
+        [HttpGet("comment/{id}")]
+        public async Task<ActionResult<ItemComment>> GetCommentById(int id)
+        {
+            var comment = await _itemCommentRepository.GetById(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            return Ok(comment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddComment([FromBody] ItemComment comment)
+        {
+            comment.CreateAt = DateTime.UtcNow;
+            await _itemCommentRepository.Add(comment);
+            return CreatedAtAction(nameof(GetCommentById), new { id = comment.Id }, comment);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateComment(int id, [FromBody] ItemComment comment)
+        {
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingComment = await _itemCommentRepository.GetById(id);
+            if (existingComment == null)
+            {
+                return NotFound();
+            }
+
+            await _itemCommentRepository.Update(comment);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteComment(int id)
+        {
+            var comment = await _itemCommentRepository.GetById(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            await _itemCommentRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Dailydev Challenge/DailyDev/Models/ItemComment.cs
@@ -0,0 +1,13 @@
+﻿namespace DailyDev.Models
+{
+    public class ItemComment
+    {
+        public int Id { get; set; }         
+        public int UserId { get; set; }      
+        public int ItemId { get; set; }      
+        public string Content { get; set; }  
+        public int? ParentId { get; set; }   
+        public DateTime CreateAt { get; set; } 
+    }
+
+}
